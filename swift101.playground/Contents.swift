2ab55e0ca1ea9494cff84caa4d2f2f19d36ab4bf
Njoku Emmanuel <kalunjoku123@gmail.com>
2020-11-04 06:32:33 +0100

    learn about increment and decrement

--- swift101.playground/Contents.swift
+++ swift101.playground/Contents.swift
@@ -1,3 +1,21 @@
-var str = "Hello Njoku Emmanuel";
+//var str = "Hello Njoku Emmanuel";
+//
+//var artin = 24/7
+//
+//let lamba = 20
+//
+//print("this is the string", str);
+//
+//print(lamba);
+//
+//print(artin);
 
-print(str);
+var counter = 1;
+
+counter += 2;
+
+print(counter);
+
+counter -= 1;
+
+print(counter);
